9c6404ca79e794b2c52320b1c89456d64f3c5bc1
billel1998 <90504468+billel1998@users.noreply.github.com>
2021-11-14 13:25:48 +0100

    BubbleSort

---
+++ BubbleSort.py
@@ -0,0 +1,22 @@
+def bubbleSort(arr):
+	n = len(arr)
+	for i in range(n):
+		for j in range(0, n-1):
+			if arr[j] > arr[j+1] :
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+A = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(A)
+
+print ("Sorted array is:")
+for i in range(len(A)):
+	print ("%d" %A[i]),
+
+
+
+
+
+
+
